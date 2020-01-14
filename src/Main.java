969ab0e969fff52fcb10cb62e3562c60e4f28553
Jeremi Lisek <jeremi.lisek@gmail.com>
2020-01-14 15:47:35 +0100

    UML added

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,53 @@ public class Main {
        openedFile.close();
    }
 
+   // 0 - imie, 1 - nazwisko, 2 - numer, 3 - ulica, 4 - nrUlicy, ew 5 - nr mieszkania, 6 - kod pocztowy 7 - urzad pocztowy
    private static void poprawienieDanych(KsiazkaAdresowa ksiazka, char[] input){
 
+        Osoba osoba = new Osoba();
+        String numerTelefonu = new String();
+        StringBuilder buff = new StringBuilder();
+        int i = 0;
+       for (char c : input) {
+           if(c == ' ' || c == '\n'){
+               switch (i) {
+                   case 0:
+                       osoba.setImie(buff.toString());
+                       break;
+                   case 1:
+                       osoba.setNazwisko(buff.toString());
+                       break;
+                   case 2:
+                       numerTelefonu = buff.toString();
+                   case 3:
+                       osoba.adres.setUlica(buff.toString());
+                       break;
+                   case 4:
+                       osoba.adres.setNrDomu(buff.toString());
+                       i++;
+                       break;
+                   case 5:
+                       osoba.adres.setNrMieszkania(buff.toString());
+                       break;
+                   case 6:
+                       osoba.adres.setKodPocztowy(buff.toString());
+                       break;
+                   case 7:
+                       osoba.adres.setUrzadPocztowy(buff.toString());
+                       ksiazka.ultraSetter(numerTelefonu, osoba);
+                       i = -1;
+                       break;
+               }
+               i++;
+               buff.delete(0, buff.capacity());
+           }else if (c == '/'){
+               osoba.adres.setNrDomu(buff.toString());
+               buff.delete(0, buff.capacity());
+               i++;
+           }else{
+               buff.append(c);
+           }
+       }
    }
 
    private static void wczytywanie(String fileName, KsiazkaAdresowa ksiazka) throws IOException {
@@ -33,7 +78,7 @@ public class Main {
 
     public static void main(String[] args){
         KsiazkaAdresowa ksiazka = new KsiazkaAdresowa();
-
+        /*
         ksiazka.wstaw();
         Adres adres = new Adres("a", "b", "c","d", "e");
         Osoba osoba = new Osoba("j", "b", adres);
@@ -44,5 +89,13 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+         */
+        try {
+            wczytywanie("ksiazka.txt", ksiazka);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(ksiazka.doWyswietlenia());
     }
 }
